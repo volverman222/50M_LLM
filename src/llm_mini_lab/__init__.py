81eb627efda3ecec2f42c3e5b4cb8ceebe0b5d32
Oscar Manuel Hernandez Hernandez <oscarmanuelhernandezhernandez@MacBook-Air-de-Oscar.local>
2026-09-09 23:37:16 +0200

    Keep source and tests out of published update

--- src/llm_mini_lab/__init__.py
+++ src/llm_mini_lab/__init__.py
@@ -2,13 +2,10 @@
 
 from .model import GPTModel, TransformerBlock
 from .hellaswag import evaluate_hellaswag, render_hellaswag_example
-from .benchmarks import evaluate_benchmark, evaluate_benchmark_suite
 
 __all__ = [
     "GPTModel",
     "TransformerBlock",
     "evaluate_hellaswag",
     "render_hellaswag_example",
-    "evaluate_benchmark",
-    "evaluate_benchmark_suite",
 ]
